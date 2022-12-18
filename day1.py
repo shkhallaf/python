1e0518ea59d8a4880bc59f48a84bfbd191af36ca
shimaa <shimaakhallaf507@gmail.com>
2022-12-18 14:43:23 +0200

    python day 1

---
+++ day1.py
@@ -0,0 +1,161 @@
+# # # x = "shimaa"
+# # # print (x)
+# # # print ("hello world")
+# # # _a1sdf = 23 
+# # # print (a1sdf)
+# # # print ("hello world")
+# # # False = "str"
+# # #isVisible = True 
+# # # isVisible = False  #  
+# # # isVisible = False
+# # age = 23
+# # age= age +5 # age +=5
+# # result = 1
+# # salary = 1000
+# # print (type(result))
+# # result = int (result)
+# # result = age /salary
+# # print (type(result))
+# # print (2**3)
+
+# # # age = float(age)
+# # # print(type(age))
+# # # print (age//salary)
+
+
+
+# num = int (input("Enter the number : "))
+# print(num)
+# print (type(num)) #15
+# if num%3 == 0 and num%5==0:
+#     print ("my body")
+#     print ("divided by 5 and 3")
+# else:
+#     if num %3 == 0:
+#         print("divisible only by 3")
+#     elif num %5 ==0:
+#         print("divisible only by 5") 
+#     else:
+#         print ("not divisible by 3 nor 5")
+# print("outside")
+
+# print (1.2 and 1.4)
+# print (not -11111  and 1) 
+# print ((True or 0) and 1 )
+# print (((1+2)*4)/8)
+# if 0 == False: #12356 == 1 
+#     print ("equal ")
+# else:
+#     print ("not equal")
+# print (123 == "123")
+# print (13456 == True)
+sum  = 0 
+# for i in range (0,10):
+#     num = int(input("Enter number : "))
+#     sum +=num 
+# print (sum)
+# i = 0 
+# while i < 10:
+#     num = int(input("Enter number : "))
+#     sum +=num 
+#     i+=1
+# print (sum)
+'''multiline comment 
+'''
+# name = "shimaa"
+# name ='ahmed'
+# mulilineString = '''shimaa mohammed 
+# khallaf'''
+# l = [1,1,2,3]
+# for ele in mulilineString:
+#     print (ele)
+# print(len(mulilineString))
+# for i in range(0,len(mulilineString)):
+#     print(mulilineString[i])
+# for hamada in mulilineString :
+#     print(hamada)
+# print(mulilineString.count("m"))
+# print(mulilineString[::-1])
+# print(mulilineString[-1])
+# print(mulilineString.count("m"))
+# print(mulilineString.index("m"))
+
+
+# str = "'my name is {name} , I love {sub}'"
+# print(str.format(name = "shimaa" , sub ="python"))
+# print(str.format(sub =  "Linux" , name = "remon"))
+
+
+# name = "rania"
+# str = f'hello ya {name}'
+# # name ="x"
+# print(str)
+# num = input ("enter your number : ")
+# while not num.isdigit():
+#     num = input ("enter your number : ")
+# num = int(num)
+# print(num + 5)
+# print(type(num))
+
+# name = "Ahmed  mohammed"
+# name=name.capitalize()
+# print (name[3].isupper())
+# print(name.lower())
+
+# str = input ("Enter your name : ")
+# print(str)
+# # str = str.strip("@ ").replace("@" , "a") 
+# # print (str)
+# counta =  str.strip("@ ").replace("@" , "a").count("a") 
+# print(counta)
+
+# counta , countb = str.count("a") , str.count("b")
+# a , b =[1,2]
+# name = "omnia"+ " mohammed " *5 
+# print (name)
+# print(name.isalpha())
+# c = complex(4,5)
+# print(c)
+# num = 12.745
+# print (round(num))
+# lis = ["shimaa" , 23 , 100.5 , ["python" , "VC"] , (1,2,3) , (1,2,3)]
+# print(lis)
+# lis.append("10")
+# lis[0] = "updated"
+# print(lis)
+# lis.insert(100,"insert in index 0")
+# print(lis)
+# ele = lis.pop(0)
+# ele2 = lis.remove((1,2,3))
+# print(ele)
+# print(ele2)
+# print (lis)
+# for listitem in lis:  
+#     print(listitem)
+# print(len(lis))
+# for hazlqoom in range (0,len(lis)):
+#     print(lis[hazlqoom])
+# if "shimaa" in lis:
+#     print("exist")
+# else:
+#     print("doesn't exist ")
+lis2 = list([1,7,2,3])
+lis3 = [1,2,"shimaa"]
+lis4 = lis2 +lis3
+print(lis4)
+lis2.extend(lis3)
+lis3+=lis2
+print(lis2)
+print(lis3)
+# lis2.sort(reverse=True)
+# print(7 in lis2)
+
+# print(lis2)
+# st = "shimaa"
+# st = list(st)
+# print(type(st))
+# print(st)
+# st = str(st)
+# print(type(st))
+# print(st)
+print([] == False)
